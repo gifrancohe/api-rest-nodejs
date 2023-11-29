36485a1f7f89f80b35f734073964a56e8f96bb8f
Giovanny Franco <giovanny.franco@keyrus.com>
2023-11-29 16:36:04 -0500

    Create two new routes to generate token and payments methos for payu api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const movies = require('./movies')
 const crypto = require('crypto')
+const tokens = require('./utils/tokens')
 const { validateMovie, validationPartialMovie } = require('./schemas/movies')
 
 const app = express()
@@ -89,6 +90,31 @@ app.options('/movies/:id', (req, res) => {
   }
   res.send(200)
 })
+
+app.get('/payments-api/4.0/service.payment', (req, res) => {
+  const { callback, public_key, account_id, list_id, _ } = req.query;
+
+  const jsonResponse = `${callback}([{id:11, name:'MASTERCARD'},{id:99, name:'TEST_CREDIT_CARD'},{id:44, name:'VISA'},{id:22, name:'DINERS'},{id:12, name:'AMEX'},{id:45, name:'VISA_DEBIT'},{id:34, name:'OTHERS_CASH'},{id:23, name:'CODENSA'},{id:36, name:'BANK_REFERENCED'},{id:25, name:'PSE'},{id:37, name:'EFECTY'},{id:26, name:'ACH_DEBIT'},{id:27, name:'CASH_ON_DELIVERY'},{id:733, name:'BNPL'},{id:40, name:'CMR'},{id:41, name:'MASTERPASS'},{id:43, name:'MASTERCARD_DEBIT'},{id:32, name:'LENDING_INSTALLMENTS'},])`;
+
+  res.send(jsonResponse);
+});
+
+app.get('/payments-api/4.0/service.token', (req, res) => {
+  const { callback, _card } = req.query
+  const payerId = _card ? _card.payer_id : undefined
+  const method = _card ? _card.method : undefined
+  const token = tokens.generateUuid()
+
+  const jsonResponse = `${callback}({
+    "token": ${token},
+    "name": "APPROVED",
+    "payer_id": ${payerId},
+    "method": ${method},
+    "document": "null"
+  })`;
+
+  res.send(jsonResponse);
+});
 
 const PORT = process.env.PORT ?? 1234
 
